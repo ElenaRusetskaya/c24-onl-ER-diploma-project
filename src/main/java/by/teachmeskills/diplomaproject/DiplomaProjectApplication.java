c8db7e63704c4692e6eced045e3a51a3bbb72117
ElenaRusetskaya <michaelislen@gmail.com>
2024-03-05 16:24:25 +0300

    init: start

---
+++ src/main/java/by/teachmeskills/diplomaproject/DiplomaProjectApplication.java
@@ -0,0 +1,11 @@
+package by.teachmeskills.diplomaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectApplication.class, args);
+    }
+}
